1b6a186f0afd64cbdd90899339c43ef44053b39b
Bishal Moktan <busial.mok9@gmail.com>
2023-03-25 00:12:07 +0530

    Change database connection

--- db.js
+++ db.js
@@ -3,8 +3,8 @@ import * as dotenv from 'dotenv';
 dotenv.config();
 
 export const db = mysql.createConnection({
-  host: 'sql12.freemysqlhosting.net',
-  user: 'sql12606549',
-  password: process.env.DB_PW,
-  database: 'sql12606549',
+  host: process.env.MYSQL_DB,
+  user: process.env.MYSQL_USER,
+  password: process.env.MYSQL_PASSWORD,
+  database: process.env.MYSQL_DB,
 });
